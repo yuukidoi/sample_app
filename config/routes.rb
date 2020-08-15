bdbb5c48f9641614243437042357d3a7653a53fc
doiyuki <dogrunruns@yahoo.co.jp>
2020-08-15 10:12:01 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   
+  get 'users/new'
   get 'foo/bar'
   get 'foo/buz'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-
+  #get 'static_pages/home'
+  #get 'static_pages/help'
+  get'/help', to: 'static_pages#help'
+#  get 'static_pages/about'
+get '/about',  to: 'static_pages#about' 
+#get 'static_pages/contact'
+get '/contact', to: 'static_pages#contact'
   root 'static_pages#home'
+
+  get '/signup', to: 'users#new'
   
 end
